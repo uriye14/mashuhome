0b99fdb862ab418c7af8df0bb9e9a777812ddc76
Mashuhome <yechunhao14@gmail.com>
2026-05-15 12:50:50 +0800

    🎨 更新图片：核桃花生奶

--- 启动Agent.bat
+++ 启动Agent.bat
@@ -3,4 +3,4 @@ set HTTP_PROXY=http://127.0.0.1:7890
 set HTTPS_PROXY=http://127.0.0.1:7890
 set ALL_PROXY=http://127.0.0.1:7890
 cd /d D:\YCH\AI\mashuhome
-claude
+claude --dangerously-skip-permissions
